ac1b1fb403a4d805f3f3913d99baad8077c3cf92
Leandra <andrawisnu60@gmail.com>
2025-01-07 15:23:10 +0700

    Splash Screen Added

---
+++ app/src/main/java/com/example/esemkacorporation/Splash.kt
@@ -0,0 +1,23 @@
+package com.example.esemkacorporation
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.esemkacorporation.databinding.ActivitySplashBinding
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val binding = ActivitySplashBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@Splash, Login::class.java))
+        }, 500)
+    }
+}
