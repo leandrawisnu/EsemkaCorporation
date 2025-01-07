17982595f8c89a033b20096d35a348ce4b95e51d
Leandra <andrawisnu60@gmail.com>
2025-01-07 14:27:33 +0700

    Tweaks

--- app/src/main/java/com/example/esemkacorporation/Subordinate.kt
+++ app/src/main/java/com/example/esemkacorporation/Subordinate.kt
@@ -11,10 +11,5 @@ class Subordinate : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_subordinate)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
--- app/src/main/java/com/example/esemkacorporation/WorksWith.kt
+++ app/src/main/java/com/example/esemkacorporation/WorksWith.kt
@@ -11,10 +11,5 @@ class WorksWith : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_works_with)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
